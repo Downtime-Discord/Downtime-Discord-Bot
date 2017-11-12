511d4659b7e516fc8c5a6d1cc6066ea09821566f
mxun <mxun@eecs.wsu.edu>
2017-11-11 18:47:48 -0800

    rename f to troll file

---
+++ modules/troll.js
@@ -0,0 +1,12 @@
+module.exports.F = function(message){
+  message.channel.send("Respects Paid");
+};
+
+module.exports.rip = function(message){
+  message.channel.send("Rest in Pieces");
+};
+
+module.exports.tablethrow = function(message){
+  message.channel.send("(╯°□°）╯︵ ┻━┻");
+  message.channel.send("https://imgur.com/gallery/OHTVdwg");
+};
